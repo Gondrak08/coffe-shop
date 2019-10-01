a2b2127453c4a9b840f6cf5e86c29801155a5b8b
Gondrak08 <vitor.alecrim.p@gmail.com>
2019-10-01 15:54:39 -0300

    alpha0.5

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,15 +11,16 @@ import Info from '../components/Home/info'
 const AboutPage = ({data}) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <Hero img={data.img.childImageSharp.fluid} title="quem somos" />  
+      <Hero img={data.img.childImageSharp.fluid} 
+      title="quem somos" styleClass="about-background" 
+      />  
     <Info />
-    
   </Layout>
 );
 
 export const query = graphql`
   {
-    img: file(relativePath: {eq: "about-background.jpeg"}) {
+    img: file(relativePath: {eq: "about-background.jpg"}) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
